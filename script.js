e948008a639cb8fe1b40c44346373e0925c36225
saad <69530708+Saad042@users.noreply.github.com>
2023-09-03 02:02:56 +0500

    add keyboard functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const resultSpan = document.querySelector(".result-text");
+const inputField = document.getElementById("result-input");
 const operators = ["+", "-", "*", "/"];
 let isCurrentNumberFloat = false;
 let isNumberPresent = false;
@@ -7,32 +7,29 @@ let isResultCalculated = false;
 function addToDisplayString(event) {
   const input = event.srcElement.textContent;
   if (isResultCalculated) {
-    resultSpan.textContent = "";
-    isNumberPresent = false;
-    isCurrentNumberFloat = false;
-    isResultCalculated = false;
+    clearDisplayString();
   }
   if (input === ".") {
     if (!isCurrentNumberFloat) {
-      resultSpan.textContent += input;
+      inputField.value += input;
       isCurrentNumberFloat = true;
       isNumberPresent = false;
     }
   } else if (operators.includes(input)) {
     if (isNumberPresent) {
-      resultSpan.textContent += input;
+      inputField.value += input;
       isCurrentNumberFloat = false;
       isNumberPresent = false;
     }
   } else {
-    resultSpan.textContent += input;
+    inputField.value += input;
     isNumberPresent = true;
   }
 }
 
 function calculateResult() {
   if (!isResultCalculated) {
-    const inputString = resultSpan.textContent;
+    const inputString = inputField.value;
     const inputOperands = inputString.split(/[+*\/-]/g);
     const inputOperators = inputString.match(/[+*\/-]/g);
     while (inputOperands.length > 1 && inputOperands[0] !== NaN) {
@@ -48,7 +45,7 @@ function calculateResult() {
       }
     }
     if (inputOperands[0] !== "") {
-      resultSpan.textContent += ` = ${inputOperands[0]}`;
+      inputField.value += ` = ${inputOperands[0]}`;
     }
     isResultCalculated = true;
   }
@@ -80,9 +77,60 @@ function operate(num1, num2, operator) {
 }
 
 function clearDisplayString() {
-  resultSpan.textContent = "";
+  inputField.value = "";
+  isNumberPresent = false;
+  isCurrentNumberFloat = false;
+  isResultCalculated = false;
 }
 
 function removeLastEntry() {
-  resultSpan.textContent = resultSpan.textContent.slice(0, -1);
+  inputField.value = inputField.value.slice(0, -1);
+  if (!inputField.value) {
+    clearDisplayString();
+  }
 }
+
+function moveCursorToEnd() {
+  inputField.setSelectionRange(
+    inputField.value.length,
+    inputField.value.length
+  );
+}
+
+inputField.addEventListener("focus", moveCursorToEnd);
+
+inputField.addEventListener("click", moveCursorToEnd);
+
+inputField.addEventListener("keydown", (event) => {
+  const key = event.key;
+  const allowedKeys = "1234567890.+-/*";
+  if (!allowedKeys.includes(key) && key !== "Backspace") {
+    event.preventDefault();
+    if (key === "=") {
+      calculateResult();
+    }
+  } else {
+    if (isResultCalculated) {
+      clearDisplayString();
+    }
+    if (key === ".") {
+      if (!isCurrentNumberFloat) {
+        isCurrentNumberFloat = true;
+        isNumberPresent = false;
+      } else {
+        event.preventDefault();
+      }
+    } else if (operators.includes(key)) {
+      if (isNumberPresent) {
+        isCurrentNumberFloat = false;
+        isNumberPresent = false;
+      } else {
+        event.preventDefault();
+      }
+    } else if (key === "Backspace" && inputField.value.length <= 1) {
+      clearDisplayString();
+    } else {
+      isNumberPresent = true;
+    }
+  }
+});
